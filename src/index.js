e39a13cc276e7602cb1fa1a77a489d0cc1885e8f
Jackline-ke <antoninahodhiambo@gmail.com>
2023-03-26 11:02:28 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
+const content =document.querySelector("main-content");
+console.log(content);
+
+const taskList = document.querySelector("tasks");
+const taskForm = document.querySelector("create-task-form");
+
+
+taskForm.addEventListener("submit", function(event){
   // your code here
+  event.preventDefault();
+
+  const newTask = document.querySelector("#new-task-description").value;
+
+  taskList.innerHTML +=
+  <li> ${newTask}
+  <button data-action = "delete" data-matt = "powerful" data-charie = "downfield">x</button>
+  </li>
+
+  taskForm.reset();
+});
+
+taskList.addEventListener("click", function(event){
+  console.log(event.target);
+
+  if (event.target.dataset.action === "delete"){
+    event.target.parentElement.remove();
+  }
 });
